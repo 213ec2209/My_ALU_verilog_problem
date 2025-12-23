3fe50b893c557507df88d2f5dad29d0362322fc9
2013ec2209 <rajputanilkumar@gmail.com>
2025-12-23 20:30:05 +0530

    test_branch

---
+++ sources/ALU.v
@@ -0,0 +1,11 @@
+`timescale 1ns/1ps
+module ALU (
+    input  wire        clk,   // clock
+    input  wire        rst,   // reset
+    input  wire [7:0]  A,     // first operand
+    input  wire [7:0]  B,     // second operand
+    input  wire [2:0]  op,    // operation select
+    output reg  [7:0]  Y      // ALU output
+);
+//To do implement functionality
+endmodule
