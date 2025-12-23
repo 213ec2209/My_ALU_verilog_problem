c2f87ceefdc30c3c0689ff7f29fb4b2b6d3424d2
2013ec2209 <rajputanilkumar@gmail.com>
2025-12-23 11:01:06 +0530

    Update baseline: Add incomplete ALU.sv in sources/, ensure no tests directory

--- sources/ALU.sv
+++ sources/ALU.sv
@@ -1,10 +1,10 @@
-
 `timescale 1ns/1ps
 module ALU (
-
-   input  logic [7:0] A,// first input
-    input  logic [7:0] B, //second input
-    input  logic [2:0] op,//which operation
+   input  wire       clk,// input clock signal
+   input  wire        rst,   // reset
+   input  wire [7:0] A,// first input
+    input  wire [7:0] B, //second input
+    input  wire [2:0] op,//which operation
     output reg [7:0] Y //output of alu operation
 
 );
@@ -12,6 +12,5 @@ module ALU (
 //Insert internal signal declarations
  // write a case statement to perform different arithmetic and logic operations  
  
-
 
 endmodule
